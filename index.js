e3c75b7c6523b72115d2fe52e06cb483df8dc159
play.js <git@gituser.com>
2020-11-01 00:15:29 +0800

    Changing the description to Algorithms course

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-console.log("Hello, this is the calculator app")
+console.log("Hello, this is the algorithms course")
 
-let x = 2
-let y = 4
-let addition = x + y
-let substraction = x - y
-let multiplication = x * y
-let division = x / y
+//''let x = 2
+// let y = 4
+// let addition = x + y
+// let substraction = x - y
+// let multiplication = x * y
+// let division = x / y
 
-console.log("The value of " + x + " added with " + y + " is " + addition)
+// console.log("The value of " + x + " added with " + y + " is " + addition)
 
-console.log("The value of " + x + " substracted with " + y + " is " + substraction)
+// console.log("The value of " + x + " substracted with " + y + " is " + substraction)
 
-console.log("The value of " + x + " multiplied with " + y + " is " + multiplication)
+// console.log("The value of " + x + " multiplied with " + y + " is " + multiplication)
 
-console.log("The value of " + x + " divided with " + y + " is " + division)
+//console.log("The value of " + x + " divided with " + y + " is " + division)
+
+
+let num = 2
+
+while (num < 100) {
+  console.log(num)
+  num += 5
+}
