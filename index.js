3b0f8332d052376b4c61759515ee464fc431f681
play.js <git@gituser.com>
2020-11-01 00:07:50 +0800

    First time committing

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Hello, this is the calculator app")
+
+let x = 2
+let y = 4
+let addition = x + y
+let substraction = x - y
+let multiplication = x * y
+let division = x / y
+
+console.log("The value of " + x + " added with " + y + " is " + addition)
+
+console.log("The value of " + x + " substracted with " + y + " is " + substraction)
+
+console.log("The value of " + x + " multiplied with " + y + " is " + multiplication)
+
+console.log("The value of " + x + " divided with " + y + " is " + division)
