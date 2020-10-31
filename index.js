5e991a8ae8ff6e6b87f4d047f05d895862fc02cb
play.js <git@gituser.com>
2020-11-01 00:18:06 +0800

    Removing all comments

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
 console.log("Hello, this is the algorithms course")
 
-//''let x = 2
-// let y = 4
-// let addition = x + y
-// let substraction = x - y
-// let multiplication = x * y
-// let division = x / y
-
-// console.log("The value of " + x + " added with " + y + " is " + addition)
-
-// console.log("The value of " + x + " substracted with " + y + " is " + substraction)
-
-// console.log("The value of " + x + " multiplied with " + y + " is " + multiplication)
-
-//console.log("The value of " + x + " divided with " + y + " is " + division)
-
-
-let num = 2
+let num = 0
 
 while (num < 100) {
   console.log(num)
